f87779ebddcec784a1c5e1d2e0b6edf88ffe72a7
Noah Kim <noahbkim@gmail.com>
2021-01-25 10:20:27 -0800

    Update to publish

--- setup.py
+++ setup.py
@@ -8,15 +8,17 @@ root = Path(__file__).absolute().parent
 with root.joinpath("README.md").open() as fh:
     long_description = fh.read()
 
-spec = importlib.util.spec_from_file_location("curricula", str(root.joinpath("curricula")))
+spec = importlib.util.spec_from_file_location(
+    "curricula_grade_cpp",
+    str(root.joinpath("curricula_grade_cpp", "version.py")))
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
 setup(
     name="curricula",
-    version=module.__version__,
-    description="A content manager and grading toolkit for evaluating student code",
-    url="https://github.com/curriculagg/curricula",
+    version=module.version,
+    description="A set of grading addons specific to C++",
+    url="https://github.com/curriculagg/curricula-grade-cpp",
     author="Noah Kim",
     author_email="noahbkim@gmail.com",
 
@@ -25,17 +27,15 @@ setup(
     long_description_content_type="text/markdown",
 
     # Python
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 
     # Packaging
     packages=find_packages(),
     package_data={"curricula": [
-        "compile/schema/*.json",
-        "grade/include/*.hpp",
-        "grade/include/**/*.hpp",
-        "grade/gtest/*.cpp",
-        "template/**/*.md",
-        "template/**/*.html"]},
+        "include/*.hpp",
+        "include/**/*.hpp",
+        "gtest/*.cpp"]},
     include_package_data=True,
     zip_safe=False,
-    install_requires=["jinja2", "jsonschema"])
+    install_requires=[f"curricula=={module.version}", f"curricula-grade=={module.version}"])
+
